341b6bab5f4726ced7a40745c2b8fae6a99318d5
Stanislao Vargas <e_vargas.v@outlook.com>
2024-10-08 00:16:16 -0400

    Haciendo llamada  de httpRequest

--- src/ConsultarTasaDeMoneda.java
+++ src/ConsultarTasaDeMoneda.java
@@ -1,3 +1,13 @@
-public class ConsultarTasaDeMoneda {
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 
+public class ConsultarTasaDeMoneda {
+    public Moneda buscaMoneda(String monedaBase, String monedaAConvertir) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://foo.com/"))
+                .build();
+
+    }
 }
