8b566f440e78bd851e1606b4ea5ec598f2e25cb7
Stanislao Vargas <e_vargas.v@outlook.com>
2024-10-08 17:19:12 -0400

    Instanciando con clase ConsutlarTasaDeMoneda

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
 
         /*ConsultarTasaDeMoneda consulta = new ConsultarTasaDeMoneda();
         Moneda moneda = consulta.buscaMoneda("USD", "ARS");
         System.out.println(moneda);*/
+
+        Scanner lectura = new Scanner(System.in);
+        ConsultarTasaDeMoneda consulta = new ConsultarTasaDeMoneda();
+
+
 
 
     }
