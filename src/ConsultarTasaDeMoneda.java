46b6c73c1f7367203a905dab491fa41cedfc62c6
Stanislao Vargas <e_vargas.v@outlook.com>
2024-10-08 14:30:21 -0400

    Añadiendo HttpResponse y haciendo conexión con Gson

--- src/ConsultarTasaDeMoneda.java
+++ src/ConsultarTasaDeMoneda.java
@@ -1,13 +1,24 @@
+import com.google.gson.Gson;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ConsultarTasaDeMoneda {
     public Moneda buscaMoneda(String monedaBase, String monedaAConvertir) {
+
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/7a1f6c562c7eb4c9918c51ff/latest/USD"
+                + monedaBase + "/" + monedaAConvertir);
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://foo.com/"))
+                .uri(direccion)
                 .build();
 
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        return new Gson().fromJson(response.body(), Moneda.class);
     }
 }
